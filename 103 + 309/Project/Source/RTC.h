3832bc755b2d82cfe4044cd4c32e16d71dff6c89
CS19970929 <32015803+CS19970929@users.noreply.github.com>
2026-08-21 18:59:58 +0800

    rtc: clarify low-power driver API

--- 103 + 309/Project/Source/RTC.h
+++ 103 + 309/Project/Source/RTC.h
@@ -3,69 +3,55 @@
 
 #include "conf.h"
 
-
 #define RTC_BKP_DATA        0xA5A5
-#define LSE_START_TIMEOUT   ((uint16_t)0x5000) /*!< Time out for LSE start up */
-#define LSE_FREQUENT		32767
-#define TIME_ZOOM			(8*60*60)			//�����ʱ���ı���ʱ��
-#define	UNIX_TIME_YEAR		1970				//��Ԫʱ��
-#define SEC_DAY				86400L          	//һ���ж���s 
+#define LSE_FREQUENT        32767
+#define TIME_ZOOM           (8 * 60 * 60)
+#define UNIX_TIME_YEAR      1970
+#define SEC_DAY             86400L
 
-#define	Leapyear(year)		((year) % 4 == 0)	//�������ᳬ��2100��û����
-#define	Days_in_year(a) 	(Leapyear(a) ? 366 : 365)
-
-/*
-struct RTC_TIME {
-	UINT16 RTC_year;		//Ϊʲô���Ϊ16λ����Ϊ��������Ҫ16λ�����ϴ�����Ҫ
-	UINT8 RTC_mon;
-	UINT8 RTC_day;
-	UINT8 RTC_hour;
-	UINT8 RTC_min;
-	UINT8 RTC_sec;
-};
-*/
+#define Leapyear(year)      ((year) % 4 == 0)
+#define Days_in_year(a)     (Leapyear(a) ? 366 : 365)
 
 struct RTC_ELEMENT {
-	UINT16  RTC_Time_Year;
-	UINT16  RTC_Time_Month;
-	UINT16  RTC_Time_Day;
-	UINT16  RTC_Time_Hour;
-	
-	UINT16  RTC_Time_Minute;
-	UINT16  RTC_Time_Second;
-	UINT16  RTC_Alarm_Year;
-	UINT16  RTC_Alarm_Month;
-	
-	UINT16  RTC_Alarm_Day;
-	UINT16  RTC_Alarm_Hour;
-	UINT16  RTC_Alarm_Minute;
-	UINT16  RTC_Alarm_Second;
+	UINT16 RTC_Time_Year;
+	UINT16 RTC_Time_Month;
+	UINT16 RTC_Time_Day;
+	UINT16 RTC_Time_Hour;
+	UINT16 RTC_Time_Minute;
+	UINT16 RTC_Time_Second;
+	UINT16 RTC_Alarm_Year;
+	UINT16 RTC_Alarm_Month;
+	UINT16 RTC_Alarm_Day;
+	UINT16 RTC_Alarm_Hour;
+	UINT16 RTC_Alarm_Minute;
+	UINT16 RTC_Alarm_Second;
 };
 
-#define RTC_element_min     {0, 1, 1, 0,\
-								0, 0, 0, 0,\
+#define RTC_element_min     {0, 1, 1, 0, \
+								0, 0, 0, 0, \
 								0, 0, 0, 0}
 
-#define RTC_element_default {18, 10, 31, 23,\
-								59, 30, 0, 0,\
+#define RTC_element_default {18, 10, 31, 23, \
+								59, 30, 0, 0, \
 								0, 9, 1, 4}
 
-#define RTC_element_max     {99, 12, 31, 23,\
-								59, 59, 99, 12,\
+#define RTC_element_max     {99, 12, 31, 23, \
+								59, 59, 99, 12, \
 								31, 23, 59, 59}
 
 extern struct RTC_ELEMENT RTC_time;
-//extern UINT8 RTC_Faultcnt;
 
-UINT32 RTC_GetWakeupPeriodSeconds(void);
-UINT32 RTC_GetLastWakeupPeriodSeconds(void);
-UINT8 RTC_IsStopWakeup(void);
-void RTC_ClearStopWakeup(void);
-void RTC_WKTimeConfig(void);
-void RTC_DisableStopWakeup(void);
-void RTC_RestoreRunInterrupts(void);
 void Init_RTC(void);
 void App_RTC(void);
 
-#endif	/* RTC_H */
+void RTC_SetWakeupPeriodSeconds(UINT32 seconds);
+UINT32 RTC_GetWakeupPeriodSeconds(void);
+UINT32 RTC_GetLastWakeupPeriodSeconds(void);
+void RTC_WKTimeConfig(void);
+void RTC_DisableStopWakeup(void);
+void RTC_SyncAfterStop(void);
+void RTC_RestoreRunInterrupts(void);
+UINT8 RTC_IsStopWakeup(void);
+void RTC_ClearStopWakeup(void);
 
+#endif /* RTC_H */
