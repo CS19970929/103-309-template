75a2abb0b78f6cddcd83a5e7b68bc1261182f786
CS19970929 <32015803+CS19970929@users.noreply.github.com>
2026-08-21 19:17:32 +0800

    core: define compact BMS state and event model

--- 103 + 309/Project/Source/System_Monitor.h
+++ 103 + 309/Project/Source/System_Monitor.h
@@ -1,16 +1,15 @@
 #ifndef SYSTEM_MONITOR_H
 #define SYSTEM_MONITOR_H
 
-//ϵͳ�����������
+/* System error counters retained for protocol/debug compatibility. */
 enum SYSTEM_ERROR_COMMAND {
-	ERROR_AFE1 = 1,			//Ϊ�˷���ʹ�ú�������ֵ
+	ERROR_AFE1 = 1,
 	ERROR_AFE2,
 	ERROR_EEPROM_COM,
 	ERROR_SPI,
 	ERROR_UPPER,
 	ERROR_CLIENT,
 	ERROR_SCREEN,
-	
 	ERROR_WIFI,
 	ERROR_BLUETOOTH,
 	ERROR_APP,
@@ -20,7 +19,6 @@ enum SYSTEM_ERROR_COMMAND {
 	ERROR_HSE,
 	ERROR_LSE,
 	ERROR_VDEATLE_OVER,
-	
 	ERROR_BALANCED,
 	ERROR_ADC,
 	ERROR_SOC_CAIL,
@@ -28,7 +26,6 @@ enum SYSTEM_ERROR_COMMAND {
 	ERROR_RESERVED_22,
 	ERROR_TEMP_BREAK,
 	ERROR_NUM = ERROR_TEMP_BREAK,
-
 
 	ERROR_REMOVE_AFE1,
 	ERROR_REMOVE_AFE2,
@@ -38,7 +35,6 @@ enum SYSTEM_ERROR_COMMAND {
 	ERROR_REMOVE_UPPER,
 	ERROR_REMOVE_CLIENT,
 	ERROR_REMOVE_SCREEN,
-	
 	ERROR_REMOVE_WIFI,
 	ERROR_REMOVE_BLUETOOTH,
 	ERROR_REMOVE_APP,
@@ -48,7 +44,6 @@ enum SYSTEM_ERROR_COMMAND {
 	ERROR_REMOVE_HSE,
 	ERROR_REMOVE_LSE,
 	ERROR_REMOVE_VDEATLE_OVER,
-	
 	ERROR_REMOVE_BALANCED,
 	ERROR_REMOVE_ADC,
 	ERROR_REMOVE_RESERVED_21,
@@ -64,7 +59,6 @@ enum SYSTEM_ERROR_COMMAND {
 	ERROR_STATUS_UPPER,
 	ERROR_STATUS_CLIENT,
 	ERROR_STATUS_SCREEN,
-	
 	ERROR_STATUS_WIFI,
 	ERROR_STATUS_BLUETOOTH,
 	ERROR_STATUS_APP,
@@ -74,7 +68,6 @@ enum SYSTEM_ERROR_COMMAND {
 	ERROR_STATUS_HSE,
 	ERROR_STATUS_LSE,
 	ERROR_STATUS_VDEATLE_OVER,
-	
 	ERROR_STATUS_BALANCED,
 	ERROR_STATUS_ADC,
 	ERROR_STATUS_RESERVED_21,
@@ -83,73 +76,110 @@ enum SYSTEM_ERROR_COMMAND {
 	ERROR_STATUS_TEMP_BREAK,
 };
 
-
-struct SYSTEM_ERROR {	
+struct SYSTEM_ERROR {
 	UINT8 u8ErrFlag_Com_AFE1;
 	UINT8 u8ErrFlag_Com_AFE2;
 	UINT8 u8ErrFlag_Com_Can;
 	UINT8 u8ErrFlag_Com_EEPROM;
-	
 	UINT8 u8ErrFlag_Com_SPI;
 	UINT8 u8ErrFlag_Com_Upper;
 	UINT8 u8ErrFlag_Com_Client;
 	UINT8 u8ErrFlag_Com_Screen;
-	
 	UINT8 u8ErrFlag_Com_Wifi;
 	UINT8 u8ErrFlag_Com_BlueTooth;
 	UINT8 u8ErrFlag_Com_App;
 	UINT8 u8ErrFlag_CBC_CHG;
-	
 	UINT8 u8ErrFlag_Store_EEPROM;
 	UINT8 u8ErrFlag_HSE;
 	UINT8 u8ErrFlag_LSE;
 	UINT8 u8ErrFlag_Vdelta_OVER;
-	
 	UINT8 u8ErrFlag_Balanced;
 	UINT8 u8ErrFlag_ADC;
 	UINT8 u8ErrFlag_Reserved21;
 	UINT8 u8ErrFlag_Reserved22;
-	
 	UINT8 u8ErrFlag_CBC_DSG;
 	UINT8 u8ErrFlag_SOC_Cail;
 	UINT8 u8ErrFlag_TempBreak;
 	UINT8 u8Res6;
 };
 
-union System_Status {				//TODO�����⣬Heat��Cool��û��
+union System_Status {
     UINT32 all;
     struct System_Status_Flag {
-		UINT8 b1StartUpBMS			:1;		//BMS��һ�ο�����־λ����ʼΪ1��ȷ���ܴ򿪹�����Ϊϵͳ��ʼ�����				//�ڶ���8λ
-		UINT8 b1Status_MOS_PRE      :1;		//Ԥ��MOS�ܹ���״̬
-		UINT8 b1Status_MOS_CHG      :1;		//���MOS�ܹ���״̬
-		UINT8 b1Status_MOS_DSG      :1;		//�ŵ�MOS�ܹ���״̬
-
-		UINT8 b1Status_Relay_PRE    :1;		//Ԥ��̵�������״̬
-		UINT8 b1Status_Relay_CHG    :1;		//�ֿڳ��̵�������״̬
-		UINT8 b1Status_Relay_DSG    :1;		//�ֿڷŵ�̵�������״̬
-		UINT8 b1Status_Relay_MAIN   :1;		//ͬ�����̵�������״̬
-
-		UINT8 b1Status_ReservedHeat         :1;		//���ȹ���״̬					//��һ��8λ
-		UINT8 b1Status_ReservedCool         :1;		//���书��״̬
-		UINT8 b1Status_AFE1         :1;		//AFE1״̬
-		UINT8 b1Status_AFE2	        :1;		//AFE2״̬
-
-		UINT8 b1Status_Balance		:1;		//���⹦��״̬
-		UINT8 b1Status_ToSleep		:1;		//������������״̬
-		UINT8 b1Status_BnCloseIO	:1;		//�������MOS�رձ�־λ
-		UINT8 b1Status_ReservedHeatCloseIO	:1;		//����ʱ�ر�MosRelay
-		
-		UINT8 b1Status_SysLimits	:1;		//res						//���ĸ�8λ
-		UINT8 b1Status_CBCCloseIO	:1;		//��ǹ����ǿ�ƹر�����������ģ��ǿ�ƹر�����
-		UINT8 b1Status_DriverExtCtrl:1;		//����תΪ�ⲿ���ƣ�����������ã�����
-		UINT8 bRcved6				:1;		//res
-
-		UINT8 b4Status_ProjectVer	:4;		//��¼һЩ��Ŀ��Ϣ��Ŀǰ���ߴ���Ϊ1�����Ϊ0
-
-		UINT8 bRcved11				:8;		//res						//������8λ
+		UINT8 b1StartUpBMS             :1;
+		UINT8 b1Status_MOS_PRE         :1;
+		UINT8 b1Status_MOS_CHG         :1;
+		UINT8 b1Status_MOS_DSG         :1;
+		UINT8 b1Status_Relay_PRE       :1;
+		UINT8 b1Status_Relay_CHG       :1;
+		UINT8 b1Status_Relay_DSG       :1;
+		UINT8 b1Status_Relay_MAIN      :1;
+		UINT8 b1Status_ReservedHeat    :1;
+		UINT8 b1Status_ReservedCool    :1;
+		UINT8 b1Status_AFE1            :1;
+		UINT8 b1Status_AFE2            :1;
+		UINT8 b1Status_Balance         :1;
+		UINT8 b1Status_ToSleep         :1;
+		UINT8 b1Status_BnCloseIO       :1;
+		UINT8 b1Status_ReservedHeatCloseIO :1;
+		UINT8 b1Status_SysLimits       :1;
+		UINT8 b1Status_CBCCloseIO      :1;
+		UINT8 b1Status_DriverExtCtrl   :1;
+		UINT8 bRcved6                  :1;
+		UINT8 b4Status_ProjectVer      :4;
+		UINT8 bRcved11                 :8;
      }bits;
 };
 
+/*
+ * BMS core state is deliberately small. It separates measured facts,
+ * protection decisions, requested actuator state and actual hardware state.
+ */
+#define BMS_MOS_BLOCK_CELL_VOLTAGE   (1UL << 0)
+#define BMS_MOS_BLOCK_CURRENT        (1UL << 1)
+#define BMS_MOS_BLOCK_TEMPERATURE    (1UL << 2)
+#define BMS_MOS_BLOCK_SHORT_CIRCUIT  (1UL << 3)
+#define BMS_MOS_BLOCK_SOC_LOW        (1UL << 4)
+#define BMS_MOS_BLOCK_USER           (1UL << 5)
+#define BMS_MOS_BLOCK_AFE            (1UL << 6)
+
+#define BMS_CORE_EVT_FAULT_CHANGED        (1UL << 0)
+#define BMS_CORE_EVT_MOS_REQUEST_CHANGED  (1UL << 1)
+#define BMS_CORE_EVT_MOS_ACTUAL_CHANGED   (1UL << 2)
+#define BMS_CORE_EVT_AFE_RECOVERED        (1UL << 3)
+#define BMS_CORE_EVT_CONFIG_CHANGED       (1UL << 4)
+#define BMS_CORE_EVT_STORAGE_DIRTY        (1UL << 5)
+#define BMS_CORE_EVT_WAKEUP               (1UL << 6)
+#define BMS_CORE_EVT_SLEEP_BLOCK_CHANGED  (1UL << 7)
+
+struct BMS_MEASUREMENT_STATE {
+	UINT16 cellMinMv;
+	UINT16 cellMaxMv;
+	UINT16 chargeCurrentA10;
+	UINT16 dischargeCurrentA10;
+	UINT16 socPercent;
+};
+
+struct BMS_PROTECTION_STATE {
+	UINT32 swChargeBlock;
+	UINT32 swDischargeBlock;
+	UINT32 hwChargeBlock;
+	UINT32 hwDischargeBlock;
+};
+
+struct BMS_MOS_STATE {
+	UINT8 chargeRequest;
+	UINT8 dischargeRequest;
+	UINT8 chargeActual;
+	UINT8 dischargeActual;
+};
+
+struct BMS_CORE_STATE {
+	struct BMS_MEASUREMENT_STATE measurement;
+	struct BMS_PROTECTION_STATE protection;
+	struct BMS_MOS_STATE mos;
+	UINT32 sleepBlockReason;
+};
 
 extern volatile struct SYSTEM_ERROR System_ErrFlag;
 extern volatile union System_Status s_system_status;
@@ -164,4 +194,23 @@ UINT8 SystemRuntime_IsChargeMosOpen(void);
 UINT8 SystemRuntime_IsDischargeMosOpen(void);
 UINT32 SystemRuntime_GetStatusSnapshot(void);
 
-#endif	/* SYSTEM_MONITOR_H */
+void BmsCore_Init(void);
+void BmsCore_UpdateMeasurement(UINT16 cell_min_mv,
+							   UINT16 cell_max_mv,
+							   UINT16 charge_current_a10,
+							   UINT16 discharge_current_a10,
+							   UINT16 soc_percent);
+void BmsCore_SetProtectionBlocks(UINT32 sw_charge_block,
+								UINT32 sw_discharge_block,
+								UINT32 hw_charge_block,
+								UINT32 hw_discharge_block);
+UINT8 BmsCore_SetMosRequest(UINT8 charge_on, UINT8 discharge_on);
+void BmsCore_SetSleepBlockReason(UINT32 reason);
+UINT32 BmsCore_GetSleepBlockReason(void);
+void BmsCore_GetState(struct BMS_CORE_STATE *state);
+
+void BmsEvent_Set(UINT32 events);
+UINT32 BmsEvent_Take(UINT32 mask);
+UINT32 BmsEvent_Peek(void);
+
+#endif /* SYSTEM_MONITOR_H */
